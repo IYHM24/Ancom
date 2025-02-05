7757d5449302748e9f1a5b089bd90036f6b22edb
Andrés C. Gutiérrez <dev.andres.gutierrez@gmail.com>
2025-02-05 17:37:43 -0500

    [responsive] - mantenimiento mejorado

--- src/components/shared/mantenimiento/mantenimiento.jsx
+++ src/components/shared/mantenimiento/mantenimiento.jsx
@@ -4,7 +4,7 @@ import mantenimientoIMG from '@/assets/portadas/mantenimiento2.png'
 export const Mantenimiento = ({pagina}) => {
     return (
         <div className='!mx-auto !container h-min-screen !p-40 flex flex-col gap-5'>
-            <div className="!mx-auto !max-w-[58rem] !text-center !flex !justify-center !flex-col">
+            <div className="!mx-auto !text-center !flex !justify-center !flex-col">
                 <h2 className="!font-bold !text-5xl !leading-[1.1] sm:!text-3xl md:!text-5xl">¡Ups! Mantenimiento</h2>
                 <p className="mt-4 !text-gray-600 dark:!text-gray-400 sm:text-lg !text-lg">
                     Nuestro equipo porfesional esta construyendo este sitio 

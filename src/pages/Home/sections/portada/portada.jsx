6c2259728f388ae54bf734ac85e7e29bb297a348
Andrés C. Gutiérrez <dev.andres.gutierrez@gmail.com>
2025-02-05 18:22:07 -0500

    [responsive] - responsive configurado en las secciones de home

--- src/pages/Home/sections/portada/portada.jsx
+++ src/pages/Home/sections/portada/portada.jsx
@@ -23,8 +23,8 @@ export const Portada = () => {
                     className='max-md:!p-5 !animate-fade-in !gap-10 !mx-auto container flex min-h-[calc(100vh-3.5rem)] flex-col items-center justify-center space-y-8 py-24 text-center md:py-32'
                 >
                     <div className='flex flex-col gap-5'>
-                        <h2 className='!bg-clip-text !text-3xl !font-bold !tracking-tight sm:!text-4xl md:!text-5xl lg:!text-6xl'>Transformamos Tu Idea</h2>
-                        <p className='!text-xl text-center !text-gray-700 dark:!text-gray-300'> En Ancom diseñamos software a la medida que potencia tus operaciones. Nuestra experiencia y compromiso con la excelencia nos permiten ofrecer resultados confiables, ágiles y escalables para tu crecimiento.</p>
+                        <h2 className='!bg-clip-text !text-3xl !font-bold !tracking-tight sm:!text-4xl md:!text-5xl lg:!text-6xl text-shadow'>Transformamos Tu Idea</h2>
+                        <p className='!text-xl text-center !text-gray-700 dark:!text-gray-300 text-shadow'> En Ancom diseñamos software a la medida que potencia tus operaciones. Nuestra experiencia y compromiso con la excelencia nos permiten ofrecer resultados confiables, ágiles y escalables para tu crecimiento.</p>
                     </div>
                     <div className='flex gap-2'>
                         <Button> Explorar soluciones <FaArrowRight/> </Button>
